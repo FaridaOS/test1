10b284bfcb4532e2f2fcd06082422ba84df7ab4f
FaridaOS <54897986+FaridaOS@users.noreply.github.com>
2019-10-08 22:21:13 +0200

    Create deploy.R

---
+++ deploy.R
@@ -0,0 +1,15 @@
+library(rsconnect)
+
+setAccountInfo(name   = Sys.getenv("faridaoshiny"),
+
+
+
+               token  = Sys.getenv("F8CC90702B4D223365EB5D6F9D61E642"),
+
+
+
+               secret = Sys.getenv("UaBNA5UYhxuVaN2i6/qQYfhl037pvZOGVGfaOF1r"))
+
+
+
+deployApp()
